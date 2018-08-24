7e6458f542b59f53ccfa05a7664b45cb34d73e61
Renaud42 <renaud.4243@gmail.com>
2018-08-24 22:12:57 +0200

    Added responsive, buttons, hypertext links, sliders and adjusted/improved animations

--- js/fire-api.js
+++ js/fire-api.js
@@ -100,3 +100,32 @@ for(i = 0; i < close.length; i++) {
         }, 300);
     }
 }
+
+// Sliders
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider-item");
+  var dots = document.getElementsByClassName("slider-dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slider-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " slider-active";
+}
